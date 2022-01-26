3f0f7d3012b255b653da33cffbfc38f7b626bf35
Anirudh <Anirudh.singh@zeiss.com>
2022-01-26 07:59:46 +0100

    Bio-Med Checked In

---
+++ ConsoleApp1/ConsoleApp1/Keys/ICzmBiomed.cs
@@ -0,0 +1,22 @@
+﻿using EnigmaWrapper.Delegates;
+using System;
+
+namespace EnigmaWrapper
+{
+    public interface ICzmBiomed
+    {
+        IntPtr Instance { get; }
+        CzmBiomedResult Result { get; set; }
+
+        void Dispose();
+    }
+    public interface ICzmBiomedResult
+    {
+        IntPtr Args { get; set; }
+        IntPtr Instance { get; }
+        Action_IntPtr_EnigmaWrapper_CzmError Notify { get; set; }
+        Action_IntPtr_EnigmaWrapper_CzmError_long NotifyV2 { get; set; }
+
+        void Dispose();
+    }
+}
